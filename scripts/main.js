ea54ca8d35debf47eae68456554e282919cb05a2
zahidnub <48299523+zahidnub@users.noreply.github.com>
2019-03-07 04:58:11 +0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+var myImage=document.querySelector('img');
+
+myImage.onclick=function(){
+var mySrc=myImage.getAttribute('src');
+
+if(mySrc==='images/love.jpeg'){
+
+myImage.setAttribute('src','images/flower.jpeg');
+
+}else{
+myImage.setAttribute('src','images/love.jpeg');
+}
+
+
+}
